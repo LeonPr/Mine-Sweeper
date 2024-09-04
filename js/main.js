843441f155140d0e91d63d8b13766d5efd49a02a
LeonPr <leon@masig.co.il>
2024-09-04 20:54:46 +0300

    changes: cells css, flags, radio buttons, right click

--- js/main.js
+++ js/main.js
@@ -3,51 +3,81 @@
 
 
 const MINE = '🎇'
-
+const FLAG = '🚩'
 
 const gField = []
 const gLevel = { SIZE: 4, MINES: 2 }
 const gGame = { isOn: true, shownCount: 0, markedCount: 0, secsPassed: 0 }
+
 const gIsCellMine = false
 const resCount = 1
 const resUpdate = 2
+const gBeginner = 4
+const gMedium = 8
+const gExpert = 12
+
 
 var gMinesCount
-
+var firstClick = true
 
 function onInit() {
     gMinesCount = 0
     document.getElementById("Beginner").checked = true;
     createField()
+    /*addMines()                 first click free
+    updateFieldsMinesCount()*/
     renderField()
-    //disableContextMenu()
-
+}
+function startNewGame() {
+    gMinesCount = 0
+    firstClick = true
+    if (document.getElementById("Beginner").checked) {
+        gLevel.SIZE = gBeginner
+        gLevel.MINES = 2
+    } else if (document.getElementById("Medium").checked) {
+        gLevel.SIZE = gMedium
+        gLevel.MINES = 14
+    } else if (document.getElementById("Expert").checked) {
+        gLevel.SIZE = gExpert
+        gLevel.MINES = 32
+    }
+    createField()
+    /*addMines()                             first click free
+    updateFieldsMinesCount()*/
+    renderField()
 }
 function disableContextMenu() {
     const noContext = document.getElementById("oncontextmenu");
-    console.log('noContext', noContext);
     noContext.addEventListener("contextmenu", (e) => {
         e.preventDefault();
     })
 }
 
 function mouseClick(ele, e) {
-    console.log('e',e)
-    console.log('ele', ele);
-    if (e.type == 'click') {
-        alert('Left Click Detected!');
-    }
+
+    const eleId = ele.id + ''
+    const indxI = +eleId.substring(0, eleId.indexOf(','))
+    const indxJ = +eleId.substring(eleId.indexOf(',') + 1)
+    if (gField[indxI][indxJ].isShown) return
+
+    /*if (e.type == 'click') {
+         //alert('Left Click Detected!');
+     }*/
     if (e.type == 'contextmenu') {
-        alert('Right Click Detected!');
+        if(ele.innerHTML === FLAG){
+            ele.innerHTML = ''
+        }else{
+            ele.innerHTML = FLAG
+        }
+        
+        //alert('Right Click Detected!');
+        //onCellClickedRight(ele)
     }
+
 }
 
-function onMouseD(eEvent) {
-    console.log('eEvent', eEvent)
-}
+function onResetGameLevel(elbtn) {
 
-function startNewGame() {
-    var x = document.getElementById("Beginner").checked;
 }
 
 function createField() {
@@ -59,15 +89,12 @@ function createField() {
             var cell = {
                 minesAroundCount: 0,
                 isShown: false,
-                isMine: i % j === 0 ? true : false,
+                isMine: false,
                 isMarked: false
             }
             gField[i][j] = cell
         }
     }
-    //console.log(' createField gField', gField);
-    //mineField[4][4].isMine = true
-    updateFieldsMinesCount()
 }
 function renderField() {
     var strHTML = ''
@@ -87,15 +114,15 @@ function renderField() {
             // Add a seat title
             const title = `Cell: ${i + 1}, ${j + 1}`
 
-            strHTML += `\t<td title="${title}" class="cell ${className}" 
-                            onclick="onCellClicked(this, ${i}, ${j})" >
+            strHTML += `\t<td title="${title}" id="${i}, ${j}" class="cell ${className}" 
+                            oncontextmenu="mouseClick(this, event);return false;" onclick="onCellClicked(this, ${i}, ${j})" >
                          </td>\n`
         }
         strHTML += `</tr>\n`
     }
 
-    const elSeats = document.querySelector('.minefield-field')
-    elSeats.innerHTML = strHTML
+    const elFields = document.querySelector('.minefield-field')
+    elFields.innerHTML = strHTML
 }
 function updateFieldsMinesCount() {
     for (var i = 0; i < gField.length; i++) {
@@ -106,7 +133,17 @@ function updateFieldsMinesCount() {
         }
     }
 }
+
+
 function onCellClicked(elCell, i, j) {
+    if (firstClick) {
+        firstClick = false
+        addMines(i, j)
+        updateFieldsMinesCount()
+        renderField()
+    }
+    
+    if(elCell.innerHTML===FLAG) return
     if (gField[i][j].isMine) {
         renderCell(i, j, !gIsCellMine)
         gGame.isOn = false
@@ -114,6 +151,31 @@ function onCellClicked(elCell, i, j) {
         renderCell(i, j, gIsCellMine)
     }
 }
+
+function addMines(rowIdx, colIdx) {
+    var mineNum = 0
+    var randI = -1
+    var randJ = -1
+    if (gLevel.SIZE === gBeginner) {
+        mineNum = 2
+    } else if (gLevel.SIZE === gMedium) {
+        mineNum = 14
+    } else if (gLevel.SIZE === gExpert) {
+        mineNum = 32
+    }
+    for (var i = 0; i < mineNum; i++) {
+        randI = getRandomInt(0, gLevel.SIZE)
+        randJ = getRandomInt(0, gLevel.SIZE)
+        if (gField[randI][randJ].isMine) {
+            i--
+        } else if (rowIdx === randI && colIdx === randJ) {
+            i--
+        } else {
+            gField[randI][randJ].isMine = true
+        }
+    }
+}
+
 function onCellMarked(elCell) {
     //todo: right click check
 }
@@ -128,7 +190,14 @@ function getNearMines(rowIdx, colIdx, resType) {
                 count++
             } else if (!gField[i][j].isMine && resType === resUpdate) {
                 const elCell = document.querySelector(`[title="Cell: ${i + 1}, ${j + 1}"]`)
-                elCell.innerHTML = gField[i][j].minesAroundCount
+                gField[i][j].isShown = true
+                if (gField[i][j].minesAroundCount === 0) {
+                    elCell.classList.add('pressedEmpty')
+                    elCell.innerHTML = ''
+                } else {
+                    elCell.classList.add(`pressed${gField[i][j].minesAroundCount}`)
+                    elCell.innerHTML = gField[i][j].minesAroundCount
+                }
             }
         }
     }
@@ -139,6 +208,7 @@ function getNearMines(rowIdx, colIdx, resType) {
 }
 
 function renderCell(rowIdx, colIdx, isCellMine) {
+    if (gField[rowIdx][colIdx].isShown) return
     if (isCellMine) {
         const elMines = document.querySelectorAll('.mine')
         for (var i = 0; i < elMines.length; i++) {
@@ -146,8 +216,15 @@ function renderCell(rowIdx, colIdx, isCellMine) {
         }
     } else {
         const elCell = document.querySelector(`[title="Cell: ${rowIdx + 1}, ${colIdx + 1}"]`)
-        elCell.innerHTML = gField[rowIdx][colIdx].minesAroundCount
-        getNearMines(rowIdx, colIdx, resUpdate)
+        gField[rowIdx][colIdx].isShown = true
+        if (gField[rowIdx][colIdx].minesAroundCount === 0) {
+            elCell.innerHTML = ''
+            elCell.classList.add('pressedEmpty')
+            getNearMines(rowIdx, colIdx, resUpdate)
+        } else {
+            elCell.classList.add(`pressed${gField[rowIdx][colIdx].minesAroundCount}`)
+            getNearMines(rowIdx, colIdx, resUpdate)
+        }
     }
 }
 
